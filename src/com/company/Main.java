66fcd432c6fa17d095a127bcf4e87faa45f3fef6
P1zzAITU <Aidar_amankeldy2004@mail.ru>
2022-04-27 11:34:10 +0600

    implemented method example where have SOUT message EXAMPLE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        example();
+    }
+    public static void example(){
+        System.out.println("example");
     }
 }
